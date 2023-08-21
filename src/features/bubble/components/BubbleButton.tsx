3abdbc40abc58e7c00e23f19d7a5604472540514
João Vitor Chaves Silva <jvchavessilva@gmail.com>
2023-08-21 08:51:39 -0300

    Changing Bubble Color
    Using the right color #e76353ff

--- src/features/bubble/components/BubbleButton.tsx
+++ src/features/bubble/components/BubbleButton.tsx
@@ -7,7 +7,7 @@ type Props = ButtonTheme & {
     toggleBot: () => void
 }
 
-const defaultButtonColor = '#3B81F6'
+const defaultButtonColor = '#e76353ff'
 const defaultIconColor = 'white'
 const defaultBottom = '20'
 const defaultRight = '20'
